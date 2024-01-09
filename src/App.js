f9435e6b48ca0319c1b8a8631821e99f3093d12c
LucsSM <104170146+LucsSM@users.noreply.github.com>
2024-01-08 21:27:52 -0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import { useState } from 'react';
+import './App.css';
+import Banner from './Componentes/Banner';
+import Formulario from './Componentes/Formulario';
+
+function App() {
+
+  const [colaboradores, setColaboradores] = useState([])
+
+  const aoNovoColaboradorAdicionado = (colaborador) => {
+    console.log(colaborador)
+    setColaboradores(...colaboradores, colaborador)
+  }
+
+  return (
+    <div className="App">
+      <Banner />
+      <Formulario aoColaboradorCadastrado={colaborador => aoNovoColaboradorAdicionado(colaborador)}/>
+    </div>
+  );
+}
+
+export default App;
